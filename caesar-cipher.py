63fd760627785c2d7c961a1687fc126b195d9fe0
BlacHera <121027660+BlacHera@users.noreply.github.com>
2025-03-16 07:39:15 +0100

    used the built-in function called input()

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -5,3 +5,10 @@ def getDoubleAlphabet(alphabet):
 alphabet="ABC"
 
 print(getDoubleAlphabet(alphabet))
+
+def getMessage():
+    stringToEncrypt = input("Please enter a message to encrypt: ")
+    return stringToEncrypt
+
+message = getMessage()
+print(f"You entered: {message}")

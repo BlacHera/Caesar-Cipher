885138eefa786115c7111ff2302ae0ea61e83f7c
BlacHera <121027660+BlacHera@users.noreply.github.com>
2025-03-16 07:48:33 +0100

    Encrypting the message-This function shifts each letter in the message forward by the cipher key.

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -12,3 +12,26 @@ def getMessage():
 
 message = getMessage()
 print(f"You entered: {message}")
+
+def getCipherKey():
+    shiftAmount = input("Please enter a key (whole number from 1-25): ")
+    return shiftAmount
+
+key = getCipherKey()
+print(f"You entered key: {key}")
+
+def encryptMessage(message, cipherKey, alphabet):
+    encryptedMessage = ""
+    uppercaseMessage = message.upper()  
+    for currentCharacter in uppercaseMessage:
+        position = alphabet.find(currentCharacter)
+        newPosition = position + int(cipherKey)
+        if currentCharacter in alphabet:
+            encryptedMessage += alphabet[newPosition]
+        else:
+            encryptedMessage += currentCharacter  
+    return encryptedMessage
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+doubleAlphabet = getDoubleAlphabet(alphabet)
+print(encryptMessage("HELLO", 3, doubleAlphabet)) 
